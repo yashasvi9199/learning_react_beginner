54daf8b8509a25ccec13f7a06f3bd0f165c5ed95
Yash Haldiya <yashasvi9199@gmail.com>
2023-11-19 13:54:42 +0530

    addition to Fragments topic
    app.js file for Fragment

---
+++ Notes/App.js Files/Fragment.js
@@ -0,0 +1,14 @@
+import Fragment_01 from "./component/Fragment_01";
+import Fragment_Parent from "./component/Fragment_Parent";
+
+
+function App() {
+  return (
+    <div className="App">
+      <Fragment_Parent />
+      {/* <Fragment_01 /> */}
+    </div>
+  );
+}
+
+export default App;
