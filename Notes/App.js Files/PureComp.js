5fdd31d6bc10e5d7c4d9388f14e439fd112454e3
Yash Haldiya <yashasvi9199@gmail.com>
2023-11-19 14:53:36 +0530

    Create PureComp.js

---
+++ Notes/App.js Files/PureComp.js
@@ -0,0 +1,18 @@
+import PureComp_Parent_01 from "./component/PureComp_Parent_01";
+import PureComp_Parent_02 from "./component/PureComp_Parent_02";
+import PureComp_Parent_03 from "./component/PureComp_Parent_03";
+import PureComp_Parent_04 from "./component/PureComp_Parent_04";
+
+
+function App() {
+  return (
+    <div className="App">
+      {/* <PureComp_Parent_01 /> */}
+      {/* <PureComp_Parent_02 /> */}
+      {/* <PureComp_Parent_03 /> */}
+      <PureComp_Parent_04 />
+    </div>
+  );
+}
+
+export default App;
