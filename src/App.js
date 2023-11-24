7de838b082c1af4a42ea7b02d339cce42b10482e
Yash Haldiya <yashasvi9199@gmail.com>
2023-11-24 16:24:54 +0530

    global changes for App.js

--- src/App.js
+++ src/App.js
@@ -1,14 +1,8 @@
-import ErrorHandling_child from "./component/ErrorHandling_child";
-import ErrorHandling_parent from "./component/ErrorHandling_parent";
-
-
 
 function App() {
   return (
     <div className="App">
-      <ErrorHandling_child>
-        <ErrorHandling_parent />
-      </ErrorHandling_child>
+      
     </div>
   );
 }
